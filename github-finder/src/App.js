670d2b1e825871eb3fb8b95b100901fefb67d0eb
Sanne <SanneSVisser@gmail.com>
2021-04-04 16:49:05 +0200

    .

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 import './App.css';
+import Home from './components/pages/Home'
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import User from './components/users/User';
+import NotFound from './components/pages/NotFound'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello world!</h1>
-      </div>
-    );
-  }
+import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
 
+const App = () => {
+
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+
+    </GithubState>
+
+  );
 }
 
 export default App;
