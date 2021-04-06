5c33b9a4ce8bc54b3b5ed8870f6d3ce9c4af34ab
Sanne <SanneSVisser@gmail.com>
2021-04-06 17:10:44 +0200

    Changed configs

--- contact-keeper/backend/server.js
+++ contact-keeper/backend/server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('../frontend/build'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/../frontend/build/index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
